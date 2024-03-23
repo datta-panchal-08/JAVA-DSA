f716335d66eb9645010c7158bbe94c4750319d0c
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-03-23 21:53:17 +0530

    Program - 01 - Sum Of Two Numbers

---
+++ Variable & Data Structure/Practice_Questions/program_01.java
@@ -0,0 +1,14 @@
+package Practice_Questions;
+
+// Sum Of a & b
+
+public class program_01 {
+    public static void main(String[] args) {
+        int firstVal = 13;
+        int secondVal = 15;
+
+        int sum  = firstVal + secondVal;
+        System.out.println("Sum Of Two Numbers Is : "+sum);
+
+    }
+}
