61e2b86b537d7c652ceb4d1729d3602fabb2c9f3
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-03-23 21:40:10 +0530

    Declaring Variables In Java

---
+++ Variable & Data Structure/Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    public static void main(String[] args) {
+        int a  = 13 ;
+        int b = 14 ;
+        System.out.println(a);
+        System.out.println(b);    
+    }
+}
