510be7930ef6e7c2efa10ab93714c9f68890f757
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-03-23 21:46:46 +0530

    Data Types In Java

---
+++ Variable & Data Structure/DataTypes.java
@@ -0,0 +1,37 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        // PRIMITIVE DATA TYPES
+        
+        // byte - 1byte
+        byte  a = 13;
+        System.out.println("This Is byte : "+ a);
+        
+        // short - 2byte
+        short  b = 15;
+        System.out.println("This Is short : "+ b);
+        
+        // int - 4byte
+        int  c = 17;
+        System.out.println("This Is int : "+ c);
+        
+        // long - 8byte
+        long  d = 19;
+        System.out.println("This Is long : "+ d);
+        
+        // float - 4byte
+        float  e = 21;
+        System.out.println("This Is float : "+ e);
+        
+        // double - 8byte
+        double  f = 23;
+        System.out.println("This Is double : "+ f);
+        
+        // char - 2byte
+        char  g = 'A';
+        System.out.println("This Is char : "+ g);
+        
+        // boolean - 1byte
+        boolean  h = true;
+        System.out.println(h);
+    }
+}
