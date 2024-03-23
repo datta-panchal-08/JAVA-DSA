24b7405f2522532ed15c5fc2b410d7bb01267bc9
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-03-23 22:08:36 +0530

    Taking Input From User By Using Scanner

---
+++ Variable & Data Structure/Inputs.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        System.out.print("Enter First Number : ");
+        int a = sc.nextInt();
+        System.out.println("You Entered : "+a);
+        System.out.print("Enter Second Number : "); 
+        int b = sc.nextInt();
+        System.out.println("You Entered : "+b);
+    }
+}
