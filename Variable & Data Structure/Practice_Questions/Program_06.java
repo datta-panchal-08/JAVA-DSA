e7726df5ca33698fbbdc573b98e2f8e0ee7cd69c
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-03-24 21:04:07 +0530

    Program - 06 - Average Of Three Numbers

---
+++ Variable & Data Structure/Practice_Questions/Program_06.java
@@ -0,0 +1,11 @@
+package Practice_Questions;
+// AVERAGE OF THREE NUMBERS
+public class Program_06 {
+    public static void main(String[] args) {
+        int a  = 45 ;
+        int b  = 23 ;
+        int c  = 12 ;
+        int avg = (a+b+c)/3;
+        System.out.println("Average Of Three Numbers Is :  "+avg);
+    }
+}
