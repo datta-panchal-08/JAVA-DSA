eb1a4771f64b9a9afd9215a070405381faab807e
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-03-24 21:00:22 +0530

    Program - 05 - Area Of Square

---
+++ Variable & Data Structure/Practice_Questions/Program_05.java
@@ -0,0 +1,10 @@
+package Practice_Questions;
+// AREA OF SQUARE 
+public class Program_05 {
+    public static void main(String[] args) {
+        int side = 2;
+
+        int areaOfSquare = side * side ;
+        System.out.println("Area Of Square Is : "+areaOfSquare);
+    }
+}
