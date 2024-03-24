70091e623a800beeb70d57a49543eb6688023c3a
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-03-24 21:13:47 +0530

    Learned If Else Statements

---
+++ Conditionals/Conditional_IfElse.java
@@ -0,0 +1,14 @@
+package Conditionals;
+
+public class Conditional_IfElse {
+    public static void main(String[] args) {
+        int age  = 22;
+        if(age >= 18)
+        {
+            System.out.println("Adult : Vote , Drive");
+        }
+        else{
+            System.out.println("Not Adult");
+        }
+    }
+}
