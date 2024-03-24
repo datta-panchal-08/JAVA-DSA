fa2d10636352d9173dae4dce716a23e79a461c7f
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-03-24 20:58:14 +0530

    Learned About  Type Casting

---
+++ Variable & Data Structure/Type_casting.java
@@ -0,0 +1,7 @@
+public class Type_casting {
+    public static void main(String[] args) {
+        long b  = 14 ;
+        int a  = (int) b ; // Type Casting 
+        System.out.println(a);
+    }
+}
