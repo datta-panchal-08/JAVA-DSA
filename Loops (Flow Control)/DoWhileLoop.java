337fc06f23b2c31c848a9c7b9e5c91bbc2da4712
Dattatray Venkat Panchal <panchaldatta1508@gmail.com>
2024-03-27 22:03:30 +0530

    Leraned About do-while loops

---
+++ Loops (Flow Control)/DoWhileLoop.java
@@ -0,0 +1,9 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int counter = 0;
+        do{
+          System.out.println("Hello");
+          counter++;
+        }while(counter<10); 
+    }
+}
