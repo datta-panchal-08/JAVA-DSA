d981831886735476addcd176e3ecbc2e62dc47e5
Dattatray Venkat Panchal <panchaldatta1508@gmail.com>
2024-03-28 21:11:32 +0530

    How To Implement Break Statements On Loop

---
+++ Loops (Flow Control)/Break.java
@@ -0,0 +1,13 @@
+public class Break {
+    public static void main(String[] args) {
+        for(int i = 0 ; i<=10 ;i++)
+        {
+             if(i == 6){
+                break;
+             }else{
+                System.out.println(i);
+             }
+             
+        }
+    }
+}
